1a4edc2317e86d5102733a4d1452812e863951e2
rhetesi <66908912+rhetesi@users.noreply.github.com>
2024-08-23 22:05:00 +0000

    Add async fetch

--- fnditm-fe/src/App.js
+++ fnditm-fe/src/App.js
@@ -6,7 +6,9 @@ import itemlist from './dev/db.js';
 
 function App() {
 
-const [items, setItems] = useState([{itemlist}]);
+  const [items, setItems] = useState([{itemlist}]);
+  const [itemsAsync, setItemsAsync] = useState([]);
+  const apiUrl = "http://127.0.0.1:3001";
 
   const [searchString, setSearchString] = useState([]);
 
@@ -14,12 +16,31 @@ const [items, setItems] = useState([{itemlist}]);
     setSearchString(e.target.value)
   }
 
+  // Sync fetch the items to show
   useEffect(()=>{
     if (searchString.length > 0) {
       setItems(itemlist.filter(item => item.name.toLowerCase().includes(searchString.toLowerCase())));
     } else {
       setItems(itemlist);
     }
+  },[searchString])
+
+  // Async fetch the items to show
+  useEffect(()=>{
+    const getItemsfromAPI = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        const result = await response.json();
+        if (searchString.length > 0) {
+          setItemsAsync(result.filter(item => item.name.toLowerCase().includes(searchString.toLowerCase())));
+        } else {
+          setItemsAsync(result);
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getItemsfromAPI();
   },[searchString])
 
   return (
@@ -45,9 +66,30 @@ const [items, setItems] = useState([{itemlist}]);
         <div className="container-fluid p-4">
           <div className="mt-2">
             {/* Itt lesznek egyedi kártyák */}
+            {/* Ezek a kártyák a szinkron végrehajtásból */}
+            <div>
+              <h3>Szinkron lekérés</h3>
+            </div>
             <div className="row g-4">
               {
               items.map( (item, index) => {
+                return (
+                                  
+                  <Card item={item} index={index}/>
+                  
+                )
+              })
+              }
+            </div>
+
+            {/* Ezek a kártyák az aszinkron lekérésből */}
+            <div>
+              <br></br>
+              <h3>Aszinkron lekérés</h3>
+            </div>
+            <div className="row g-4">
+              {
+              itemsAsync.map( (item, index) => {
                 return (
                                   
                   <Card item={item} index={index}/>
