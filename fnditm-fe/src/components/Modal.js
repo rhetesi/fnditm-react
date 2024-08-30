ce01f0e6321e5875ca899ad926f911d0b578f71c
rhetesi <66908912+rhetesi@users.noreply.github.com>
2024-08-30 21:54:15 +0000

    Edit the basics of the card modal

--- fnditm-fe/src/components/Modal.js
+++ fnditm-fe/src/components/Modal.js
@@ -1,47 +1,43 @@
 import cardPlaceholder from "../pics/placeholder_681_528px.png";
 
 function Modal({item}) {
+  return (
     <>
-      {/* <!-- Modal --> */}
-  
+      {/* Modal */}
+      <div className="modal fade" id="myModal" data-bs-backdrop="static" data-bs-keyboard="false">
+        <div className="modal-dialog modal-dialog-centered modal-lg">
+          <div className="modal-content">
 
-  {/* <!-- A Modal --> */}
-  <div class="modal fade" id="myModal" data-bs-backdrop="static" data-bs-keyboard="false">
-    <div class="modal-dialog modal-dialog-centered modal-lg">
-      <div class="modal-content">
-
-        {/* <!-- Modal fejléce --> */}
-        <div class="modal-header">
-          <h4 class="modal-title">Kiválasztott tárgy részletei</h4>
-        </div>
-
-        {/* <!-- Modal tartalma --> */}
-        <div class="modal-body">
-          {/* <div class="getback__check">
-            <h5 class="getback__check--title">Kiadási pénztárbizonylat</h5>
-            <div class="getback__pay">
-              <div class="getback__pay--header"><small>Pénztár fizessen az alábbiak szerint</small></div>
-              <div class="customernamediv"><small>. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-                  . . . . . . . . . . . . . . . . . . . -nak</small></div>
-              <div class="getback__pay--amount mt-2"></div>
+            {/* Modal fejléce */}
+            <div className="modal-header">
+              <h4 className="modal-title">Talált tárgy részletei</h4>
             </div>
-          </div>
-          <div class="getback__deposit mt-3"></div> */}
-          <div className="selected-item">
-            <h5 className="item__name">{item.name}</h5>
-            <div className="ite,_details">
-                <div></div>
+
+            {/* Modal tartalma */}
+            <div className="modal-body">
+                <div key={index} className="card border-secondary h-100" id={item.id}>
+                  <img
+                    src={cardPlaceholder}
+                    className="card-img-top cardPlaceholder"
+                    alt="card"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{item.name}</h5>
+                    <p className="card-text">
+                      {item.description}
+                    </p>
+                  </div>
+                </div>
             </div>
+
+            {/* Modal lábléce */}
+            <div className="modal-footer">
+              <button type="button" className="btn-btn-warning text-white" data-bs-dismiss="modal">Bezár</button>
+            </div>
+
           </div>
         </div>
-
-        {/* <!-- Modal lábléce --> */}
-        <div class="modal-footer">
-          <button type="button" class="btn btn-warning text-white" data-bs-dismiss="modal">Bezár</button>
-        </div>
-
       </div>
-    </div>
-  </div>
     </>
+  )
 }
